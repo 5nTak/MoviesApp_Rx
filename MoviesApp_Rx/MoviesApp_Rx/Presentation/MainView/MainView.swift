648586abbb9241880da6a2d8ac18ba29479f2e6b
Tak <oit5538@gmail.com>
2023-11-23 19:13:29 +0900

    refactor(UI): MainView의 검색창, 즐겨찾기 버튼 제거 (다른 탭으로 옮길 예정)
    - searchBar: SearchTab
    - starButton: AccountTab

--- MoviesApp_Rx/MoviesApp_Rx/Presentation/MainView/MainView.swift
+++ MoviesApp_Rx/MoviesApp_Rx/Presentation/MainView/MainView.swift
@@ -12,45 +12,44 @@ class MainView: UIViewController {
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.textColor = UIColor.black
-        label.font = .systemFont(ofSize: 20, weight: .bold)
-        label.text = "영화 검색"
+        label.font = .systemFont(ofSize: 25, weight: .bold)
+        label.text = "맞춤 영화"
         return label
     }()
     
     // 즐겨찾기 text 앞에 switching 가능한 starbutton
-    private let starButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("즐겨찾기", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 14)
-        
-        button.layer.borderColor = UIColor.systemGray2.cgColor
-        button.layer.borderWidth = 0.5
-        button.layer.cornerRadius = 10
-        
-        // 좀 더 깔끔한 방법 고민해보기
-        let margin: CGFloat = 10.0
-        
-        button.snp.makeConstraints {
-            $0.width.equalTo(button.titleLabel!.intrinsicContentSize.width + margin * 2.0)
-            $0.height.equalTo(button.titleLabel!.intrinsicContentSize.height + margin * 1.2)
-        }
-        return button
-    }()
+//    private let starButton: UIButton = {
+//        let button = UIButton()
+//        button.setTitle("즐겨찾기", for: .normal)
+//        button.setTitleColor(.black, for: .normal)
+//        button.titleLabel?.font = .systemFont(ofSize: 14)
+//
+//        button.layer.borderColor = UIColor.systemGray2.cgColor
+//        button.layer.borderWidth = 0.5
+//        button.layer.cornerRadius = 10
+//
+//        // 좀 더 깔끔한 방법 고민해보기
+//        let margin: CGFloat = 10.0
+//
+//        button.snp.makeConstraints {
+//            $0.width.equalTo(button.titleLabel!.intrinsicContentSize.width + margin * 2.0)
+//            $0.height.equalTo(button.titleLabel!.intrinsicContentSize.height + margin * 1.2)
+//        }
+//        return button
+//    }()
     
-    private let searchBar: UIView = {
-        let view = UIView()
-        
-        return view
-    }()
+//    private let searchBar: UIView = {
+//        let view = UIView()
+//
+//        return view
+//    }()
+//
+//    private let searchBarTextField: UITextField = {
+//        let textField = UITextField()
+//        textField.placeholder = "검색어를 입력하세요."
+//        return textField
+//    }()
     
-    private let searchBarTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "검색어를 입력하세요."
-        return textField
-    }()
-    
-        
     private lazy var collectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.setCollectionViewLayout())
         collectionView.register(
@@ -66,39 +65,39 @@ class MainView: UIViewController {
     }()
     
     private var dataSource = MainCollectionViewDataSource()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
-        setSearchBarLayout()
+//        setSearchBarLayout()
         setCollectionViewConstraints()
     }
     
     func setNavigationBar() {
         self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(customView: starButton)
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(customView: starButton)
     }
     
-    func setSearchBarLayout() {
-        view.addSubview(searchBar)
-        searchBar.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(50)
-        }
-        searchBar.addSubview(searchBarTextField)
-        searchBarTextField.snp.makeConstraints {
-            $0.horizontalEdges.equalToSuperview().inset(10)
-            $0.verticalEdges.equalToSuperview().inset(7)
-        }
-    }
+//    func setSearchBarLayout() {
+//        view.addSubview(searchBar)
+//        searchBar.snp.makeConstraints {
+//            $0.top.equalTo(view.safeAreaLayoutGuide)
+//            $0.leading.trailing.equalToSuperview()
+//            $0.height.equalTo(50)
+//        }
+//        searchBar.addSubview(searchBarTextField)
+//        searchBarTextField.snp.makeConstraints {
+//            $0.horizontalEdges.equalToSuperview().inset(10)
+//            $0.verticalEdges.equalToSuperview().inset(7)
+//        }
+//    }
     
-    func setCollectionViewLayout() {
+    func setCollectionViewConstraints() {
         view.addSubview(collectionView)
         collectionView.dataSource = dataSource
 //        collectionView.delegate = self
         collectionView.snp.makeConstraints {
-            $0.top.equalTo(searchBar.snp.bottom)
+            $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview().inset(10)
             $0.bottom.equalToSuperview()
         }
