6616e4433075a5b4440b21a314297b39f1f916d7
Tak <oit5538@gmail.com>
2023-12-04 19:19:01 +0900

    feat: UIImageView extension으로 imageBaseUrl, imageSize 프로퍼티 추가

---
+++ MoviesApp_Rx/MoviesApp_Rx/Util/UIImageView+extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIImageView+extension.swift
+//  MoviesApp_Rx
+//
+//  Created by Tak on 2023/12/04.
+//
+
+import UIKit
+
+extension UIImageView {
+    var imageSize: String {
+        return "/w500"
+    }
+    var imageBaseUrl: String {
+        return "https://image.tmdb.org/t/p"
+    }
+}
