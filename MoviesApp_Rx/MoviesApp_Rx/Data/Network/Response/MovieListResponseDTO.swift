4a76a0da5848a7d8cb3e279087f7f8077d406f08
Tak <oit5538@gmail.com>
2023-12-04 18:11:01 +0900

    feat: Movie Response DTO 구현

---
+++ MoviesApp_Rx/MoviesApp_Rx/Data/Network/Response/MovieListResponseDTO.swift
@@ -0,0 +1,54 @@
+//
+//  MovieListResponseDTO.swift
+//  MoviesApp_Rx
+//
+//  Created by Tak on 2023/12/04.
+//
+
+import Foundation
+
+struct MovieListResponse: Decodable {
+    let results: [MovieResponse]
+    let page: Int
+    let totalPages: Int
+    let totalResults: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case results, page
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+extension MovieListResponse {
+    func toMovie(movieResponse: MovieResponse) -> Movie? {
+        return Movie(
+            id: movieResponse.id,
+            title: movieResponse.title,
+            originalTitle: movieResponse.originalTitle,
+            originalLanguage: movieResponse.originalLanguage,
+            genres: movieResponse.genres,
+            adult: movieResponse.adult,
+            overview: movieResponse.overview,
+            posterPath: movieResponse.posterPath,
+            backdropPath: movieResponse.backdropPath,
+            releaseData: movieResponse.releaseDate,
+            video: movieResponse.video,
+            voteAverage: movieResponse.voteAverage,
+            voteCount: movieResponse.voteCount
+        )
+    }
+    
+    func toMovies() -> [Movie] {
+        return self.results.compactMap { toMovie(movieResponse: $0) }
+    }
+    
+    func toMovieList() -> MovieList {
+        return MovieList(
+            results: self.toMovies(),
+            page: self.page,
+            totalPages: self.totalPages,
+            totalResults: self.totalResults
+        )
+    }
+}
---
+++ MoviesApp_Rx/MoviesApp_Rx/Data/Network/Response/MovieResponseDTO.swift
@@ -0,0 +1,36 @@
+//
+//  MovieResponseDTO.swift
+//  MoviesApp_Rx
+//
+//  Created by Tak on 2023/11/25.
+//
+
+import Foundation
+
+struct MovieResponse: Decodable {
+    let id: Int
+    let title: String?
+    let originalTitle: String?
+    let originalLanguage: String?
+    let genres: [Int]?
+    let adult: Bool?
+    let overview: String?
+    let posterPath: String?
+    let backdropPath: String?
+    let releaseDate: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, overview, video, adult
+        case genres = "genre_ids"
+        case originalTitle = "original_title"
+        case originalLanguage = "original_language"
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
