a21cb8652e55c9d3deb6777395c6b583b25cc62b
Tak <oit5538@gmail.com>
2024-02-07 13:22:59 +0900

    refactor: HomeViewModel의 기존 movies 프로퍼티 및 usecase 제거 / 각 movie BehaviorRelay로 생성

--- MoviesApp_Rx/MoviesApp_Rx/Scene/Home/ViewModel/HomeViewModel.swift
+++ MoviesApp_Rx/MoviesApp_Rx/Scene/Home/ViewModel/HomeViewModel.swift
@@ -13,102 +13,18 @@ final class HomeViewModel {
     weak var coordinator: HomeCoordinator?
     
     private let movieUseCaseRx: MovieUseCaseRx
-    let discoveryMovie = PublishRelay<[Movie]>()
-    let popularMovie = PublishRelay<[Movie]>()
-    let lateMovie = PublishRelay<Movie>()
-    let trendingMovie = PublishRelay<[Movie]>()
+    let discoveryMovie = BehaviorRelay<[MovieSection]>(value: [])
+    let popularMovie = BehaviorRelay<[MovieSection]>(value: [])
+    let lateMovie = BehaviorRelay<[MovieSection]>(value: [])
+    let trendingMovie = BehaviorRelay<[MovieSection]>(value: [])
     
     let disposebag = DisposeBag()
     
-    //MARK: - 기존 코드
-//    var discoveredMovies: [Movie] = [] {
-//        didSet {
-//            discoveredMoviesHandler?(discoveredMovies)
-//        }
-//    }
-//    var popularMovies: [Movie] = [] {
-//        didSet {
-//            popularMoviesHandler?(popularMovies)
-//        }
-//    }
-//    var latestMovie: [Movie] = [] {
-//        didSet {
-//            latestMovieHandler?(latestMovie)
-//        }
-//    }
-//    var trendingMovies: [Movie] = [] {
-//        didSet {
-//            trendingMoviesHandler?(trendingMovies)
-//        }
-//    }
     var page: Int = 1
     
-//    private let movieUseCase: MovieUseCase
-//    var discoveredMoviesHandler: (([Movie]) -> Void)?
-//    var popularMoviesHandler: (([Movie]) -> Void)?
-//    var latestMovieHandler: (([Movie]) -> Void)?
-//    var trendingMoviesHandler: (([Movie]) -> Void)?
-//
     init(movieUseCaseRx: MovieUseCaseRx) {
         self.movieUseCaseRx = movieUseCaseRx
     }
-    
-//    func showContents() {
-//        showMovieDiscovery()
-//        showPopularMovies()
-//        showLatestMovie()
-//        showTrendingMovies()
-//    }
-    
-//    private func showMovieDiscovery() {
-//        movieUseCase.fetchMovieDiscovery(page: page) { result in
-//            switch result {
-//            case .success(let movies):
-//                self.discoveredMovies.append(contentsOf: movies)
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//    }
-//
-//    private func showPopularMovies() {
-//        movieUseCase.fetchPopularMoviesPage(page: page) { result in
-//            switch result {
-//            case .success(let movies):
-//                self.popularMovies.append(contentsOf: movies)
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//    }
-//
-//    private func showLatestMovie() {
-//        movieUseCase.fetchLatestMovie { result in
-//            switch result {
-//            case .success(let movie):
-//                self.latestMovie.append(movie)
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//    }
-//
-//    private func showTrendingMovies() {
-//        movieUseCase.fetchTrendingMoviesPage { result in
-//            switch result {
-//            case .success(let movies):
-//                self.trendingMovies.append(contentsOf: movies)
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//    }
-    // MARK: - RxSwift
-    
-    // Single을 asObservable화해서 옵저버블은 그냥 relay 등등 bind해주면 넘어간다
-    // accept는 relay의 onNext
-    // map은 내부 타입 변경
-    // flatmap은 옵저버블의 타입 변경 .. 아마도? observable을 반환해야함!
     
     func showMoviesRx() {
         showMovieDiscoveryRx()
